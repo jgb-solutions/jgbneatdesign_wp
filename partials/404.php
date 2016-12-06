ecf7380f3000a26881d66edf4d40bb2ddec5fe81
Jean Gérard Bousiquot <jgbneatdesign@gmail.com>
2016-12-06 18:50:33 -0500

    created the 404 page

---
+++ partials/404.php
@@ -0,0 +1,27 @@
+<section id="posts">
+	<div class="uk-panel-box uk-block-primary title-box">
+		<div class="uk-container uk-container-center">
+			<div class="uk-width-1-1">
+
+				<h1 class="uk-text-center no-margin uk-text-contrast">
+					<i class="uk-icon-close"></i> <?php single_post_title(); ?>
+				</h1>
+			</div>
+		</div>
+	</div>
+
+	<br>
+
+  	<div class="uk-container uk-container-center">
+     	<div class="uk-grid" data-uk-grid-margin>
+				<div class="uk-width-1-1 uk-text-center">
+					<p>
+						OOPS!!! The page you're trying to reach cannot be found on the site. Please return to the <a href="/">homepage</a>.
+					</p>
+				</div>
+     	</div>
+    </div>
+
+    <br>
+
+</section>
